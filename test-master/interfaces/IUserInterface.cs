5481d0d66ccae3c1507ee6abbbf6b6fcd1b27641
hojimurod07 <hojimurad2007@gmail.com>
2024-02-21 07:44:09 +0500

    men interface yozdim

---
+++ test-master/interfaces/IUserInterface.cs
@@ -0,0 +1,13 @@
+﻿using test_master.Models;
+
+namespace test_master.interfaces
+{
+    public interface IUserInterface
+    {
+        List<User> GetUsers();
+        User GetUser(int id);
+        void Add(User user);
+        void Update(User user);
+        void Delete(int id);
+    }
+}
